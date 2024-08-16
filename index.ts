5b1e1f0ca882b2365a73d391941eaaa68141a6be
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2024-08-16 20:35:12 +0300

    feat: server setting

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-// import routes from './routes';
+import { router } from './src/router';
 const app = express();
 const port = 3000;
 
-// app.use('/api', routes);
+app.use('/api', router);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
